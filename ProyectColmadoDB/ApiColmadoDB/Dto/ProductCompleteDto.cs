73b47ac9a9a71bb3497233adde6f13711cf794d5
josemiliriano <josemiliriano83@gmail.com>
2026-06-05 19:26:55 -0400

    Crud Producto

---
+++ ProyectColmadoDB/ApiColmadoDB/Dto/ProductCompleteDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiColmadoDB.Dto
+{
+    public class ProductCompleteDto
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public decimal SalePrice { get; set; }
+        public int Stock { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ ProyectColmadoDB/ApiColmadoDB/Dto/ProductDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiColmadoDB.Dto
+{
+    public class ProductDto
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public decimal SalePrice { get; set; }
+        public int Stock { get; set; }        
+        public int CategoryId { get; set; }
+    }
+}
